ff609f97888463a06eaef2bcd4ffccf95dd6615c
farhadmonavar <farhad.monavar@gmail.com>
2026-08-14 16:56:52 +0330

    feat(celery): add celery test view

--- data_import/views.py
+++ data_import/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def celery_test(request):
+    return HttpResponse('<h3>Function executed successfully</h3>')
