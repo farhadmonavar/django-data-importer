509f4b94ab306193e7c052d8c995b61e3e3a9fba
farhadmonavar <farhad.monavar@gmail.com>
2026-08-14 16:51:57 +0330

    feat(celery): add asynchronous data import task

--- dataentry/views.py
+++ dataentry/views.py
@@ -1,37 +1,60 @@
 from django.shortcuts import render, redirect
-from .utils import get_all_custom_models
+from .utils import check_csv_errors, get_all_custom_models
 from uploads.models import Upload
 from django.conf import settings
-from django.core.management import call_command
 from django.contrib import messages
+from .tasks import import_data_task, export_data_task
+from django.core.management import call_command
 
 
 def import_data(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         file_path = request.FILES.get('file_path')
         model_name = request.POST.get('model_name')
         
-        # Store this file inside the Upload model
+        # store this file inside the Upload model
         upload = Upload.objects.create(file=file_path, model_name=model_name)
         
-        # Construct the full path
+        # construct the full path
         relative_path = str(upload.file.url)
         base_url = str(settings.BASE_DIR)
         
         file_path = base_url + relative_path
         
-        # Trigger the importdata command
+        # check for the csv errors
         try:
-            call_command('import_data', file_path,model_name)
-            messages.success(request, 'Data imported successfully')
+            check_csv_errors(file_path, model_name)
         except Exception as e:
             messages.error(request, str(e))
+            return redirect('import_data')
         
+        # handle the import data task here
+        import_data_task.delay(file_path, model_name)
+        
+        # show the message to the user
+        messages.success(request, 'Your data is being imported, you will be notified once it is done.')
         return redirect('import_data')
-        
     else:
         custom_models = get_all_custom_models()
         context = {
             'custom_models': custom_models,
         }
-    return render(request, 'dataentry/importdata.html',context)
+    return render(request, 'dataentry/importdata.html', context)
+
+
+def export_data(request):
+    if request.method == 'POST':
+        model_name = request.POST.get('model_name')
+        
+        # call the export data task
+        export_data_task.delay(model_name)
+        
+        # show the message to the user
+        messages.success(request, 'Your data is being exported, you will be notified once it is done.')
+        return redirect('export_data')
+    else:
+        custom_models = get_all_custom_models()
+        context = {
+            'custom_models': custom_models,
+        }
+    return render(request, 'dataentry/exportdata.html', context)
